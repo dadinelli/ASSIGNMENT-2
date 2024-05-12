499d4340c81dc5933875d2726e493beafb3f792c
Davide <dadi@MacBook-Pro-di-Davide.local>
2024-05-12 17:13:03 +0200

    close #1

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,9 +1,19 @@
+////////////////////////////////////////////////////////////////////
+// [DAVIDE] [MARTINELLI] [2077679]
+// [MARCO] [COLA] [2079237]
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number){
-            // TODO
-            return null;
+        String res = "";
+        if(number < 3 & number > 0){
+            for(int i = 0; i < number; i++){
+                res = res + "I";
+            }
         }
+        return null;
+    }
 }
 
